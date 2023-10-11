f1b2ab6845262f040cc560df57466820e64d8a26
Juanpurpp <correo.juanm.sandoval@gmail.com>
2023-10-11 14:00:35 -0300

    update 3.c

--- labs/c2/3.c
+++ labs/c2/3.c
@@ -5,13 +5,18 @@ int main(){
   int current = 0;
   char msg[25] = "Ingrese numero:\n";
   do{
-    //printf(" %s\n ", msg);
-    scanf("%d\n", &inputs[current]);
-    //printf(" \n ");
+    printf(" %s\n ", msg);
+    scanf("%d", &inputs[current]);
+    //printf("gettedd value %d", inputs[current]);
+    printf(" \n ");
     current++;
-  }while(inputs[ current - 1 ] < 0);
-  for(int i = 0; i<25; i++){
-    printf("\nn%i:%d\n", i, inputs[i]);
+  }while(inputs[ current - 1 ] >= 0);
+
+  int pares = 0, impares = 0;
+  for(int i = 0; i<current-1; i++){
+    if(inputs[i] % 2 == 0) pares++;
+    else impares++;
   }
+  printf("\n pares: %d, impares: %d\n", pares, impares);
   return 0;
 }
